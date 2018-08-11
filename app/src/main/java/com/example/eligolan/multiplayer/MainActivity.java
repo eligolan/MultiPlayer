9e97e14de81b47ed4f8d653029a131fea7ec61c5
Eli <eligolan12@gmaiil.com>
2018-08-11 22:48:28 +0300

    fix rooms

--- app/src/main/java/com/example/eligolan/multiplayer/MainActivity.java
+++ app/src/main/java/com/example/eligolan/multiplayer/MainActivity.java
@@ -1,10 +1,19 @@
 package com.example.eligolan.multiplayer;
 
 
+import android.content.Context;
 import android.media.SoundPool;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -14,8 +23,15 @@ import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.EmailAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -23,8 +39,12 @@ public class MainActivity extends AppCompatActivity {
     SoundPool soundPool;
     int starsSoundID, winID;
     FirebaseAuth mAuth;
-    DatabaseReference usersRef;
+    DatabaseReference roomsRef;
 
+    EditText messageBox;
+    ListView listView;
+    ArrayList<Room> rooms;
+    RoomsListAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,14 +60,38 @@ public class MainActivity extends AppCompatActivity {
         }
 
         FirebaseDatabase database = FirebaseDatabase.getInstance();
-        usersRef = database.getReference("rooms");
-/*        if(usersRef == null){
-            Toast.makeText(this,"is null",Toast.LENGTH_SHORT).show();
-        }else{
-            Toast.makeText(this,"ok",Toast.LENGTH_SHORT).show();
-        }*/
-/*
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        roomsRef = database.getReference("rooms");
+        messageBox = findViewById(R.id.roomName);
+
+        rooms = new ArrayList<>();
+        listView = findViewById(R.id.listView);
+        adapter = new RoomsListAdapter(this, rooms);
+        listView.setAdapter(adapter);
+
+
+       roomsRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                rooms.clear();
+                for (DataSnapshot postSnapshot: dataSnapshot.getChildren()) {
+                    Room r = postSnapshot.getValue(Room.class);
+                    rooms.add(r);
+                }
+              try{
+                    adapter.notifyDataSetChanged();
+                }catch(Exception ex){
+                    Toast.makeText(MainActivity.this,ex.getMessage(),Toast.LENGTH_SHORT).show();
+                }
+
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+
+        /*Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setTitle("");
         setSupportActionBar(toolbar);*/
 
@@ -63,5 +107,70 @@ public class MainActivity extends AppCompatActivity {
         }
         mediaPlayer.start();*/
 
+    }
+
+    public void addRoom(View view) {
+        String myEmail = FirebaseAuth.getInstance().getCurrentUser().getEmail();
+        String key =  roomsRef.push().getKey();
+        Room r = new Room();
+        r.name = messageBox.getText().toString();
+        r.manager = myEmail;
+        roomsRef.child(key).setValue(r);
+        messageBox.setText("");
+        closeKeyboard(view);
+    }
+
+    public static class Room{
+        public String name;
+        public String manager;
+        public Room(){}
+
+        public Room(String name,String manager) {
+            this.name = name;
+            this.manager = manager;
+        }
+    }
+
+    private void closeKeyboard(View view){
+        view.clearFocus();
+        InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+    }
+
+
+
+    public static class RoomsListAdapter extends ArrayAdapter<Room> {
+
+        private ArrayList<Room> rooms;
+
+        private static class ViewHolder {
+            TextView nameTV;
+            TextView managerTV;
+        }
+
+        public RoomsListAdapter(Context context, ArrayList<Room> rooms) {
+            super(context,R.layout.rooms_list_item, rooms);
+            this.rooms = rooms;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            Room room = getItem(position);
+            ViewHolder viewHolder;
+
+            if (convertView == null) {
+                viewHolder = new ViewHolder();
+                LayoutInflater inflater = LayoutInflater.from(getContext());
+                convertView = inflater.inflate(R.layout.rooms_list_item, parent, false);
+                viewHolder.nameTV = convertView.findViewById(R.id.name);
+                viewHolder.managerTV = convertView.findViewById(R.id.manager);
+                convertView.setTag(viewHolder);
+            } else {
+                viewHolder = (ViewHolder) convertView.getTag();
+            }
+            viewHolder.nameTV.setText(room.name);
+            viewHolder.managerTV.setText(room.manager);
+            return convertView;
+        }
     }
 }
